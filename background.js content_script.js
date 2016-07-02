f46e4182458db38855221ed82719de28d1d7f84c
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-07-02 15:12:42 +0900

    modified:   background.js 	develop: closing tab

---
+++ background.js content_script.js
@@ -0,0 +1,37 @@
+var showBattery = function(){
+
+	var before,after;	//計測前後のバッテリーを記憶
+//	var timer = 1 * 60000;	//計測する時間(ミリ秒指定)
+	var timer = 5000;	//5秒(デバッグ用)
+
+	navigator.getBattery().then(function(b){
+		console.log(b.level * 100 + "%");
+		before = b.level * 100;	//計測開始時のバッテリー
+	});
+
+	var countup = function(){
+		navigator.getBattery().then(function(b){
+			console.log(b.level * 100 + "%");
+			after = b.level * 100;	//計測終了時のバッテリー
+			var result = after - before;
+			//alert("The difference is " + result + "%.");	//計測結果をalertで表示
+			chrome.runtime.sendMessage({msg: "finished", result: result}, function(response){
+				console.log("showBattery has finished!");
+			});
+		});
+	};
+
+	setTimeout(countup, timer);	//countupをtimer時間後に実行
+
+};
+
+chrome.runtime.sendMessage({msg: "sending..."}, function(response){	//background.jsにメッセージを送信
+});
+
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse){
+	    console.log('received!!');
+	    showBattery();
+	}
+);
