b8f5db94523dc21dcf8b940c5f5ce677f055a996
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-07-03 17:20:42 +0900

    modified:   background.js 	modified:   content_script.js 	develop:	url-list 	problem:	setting up twice or more times, errors occur 			(background page's console)

--- background.js
+++ background.js
@@ -1,46 +1,71 @@
-var Tab_Id;
-
 var start = function() {
-    // stub
-    var getNextUrl = function() {
-        return 'https://www.google.co.jp';
-    }
+	var Tab_Id;
+	var url_list;
+	// stub
+	var getNextUrl = function() {
+		return 'https://www.google.co.jp';
+	}
 
-    var url = getNextUrl();
+	var url = getNextUrl();
 
-    chrome.tabs.create(
-        {url: url},
-        function(tab) {
-            //console.log(tab);
-	    Tab_Id = tab.id;
-            //waitContentLoad();
+	chrome.runtime.getPackageDirectoryEntry(function(entry) {	//indexファイルの読み込み
+		entry.getFile('index.json', {create: false}, function(file_entry) {
+			file_entry.file(function(file){
+				var reader = new FileReader;
+				reader.onload = function(event){	//ファイルのreadが完了すると実行
+					url_list = JSON.parse(event.target.result);	//urlのリストを配列に格納
+				};
+				reader.readAsText(file, 'utf-8');
+			});
+		});
+	});
 
-            // message passing.(以下はタブの生成完了を待たないため，成功しない！)
-            //chrome.tabs.sendMessage(tab.id, {task: '001', msg: 'hello'});
+	chrome.tabs.create(
+		{url: url},
+		function(tab) {
+			//console.log(tab);
+			Tab_Id = tab.id;
+			//waitContentLoad();
+		}
+	);
 
-            //var before = new Date();
-            //tab.sendMessage(getTask);
-            //console.log((new Date() - before));
-        }
-    );
+	chrome.runtime.onMessage.addListener(   //content_scriptからメッセージを受信すると実行
+		function (request, sender, sendResponse) {
+			if(request.msg == "sending..."){
+				chrome.tabs.sendMessage(Tab_Id, {msg: "sending..."}, function(response){
+
+					console.log("sender: " + sender);
+				});
+			} else if(request.msg == "finished"){	//結果を表示
+				console.log("Energy Consumption: " + request.result + "%");
+				console.log("Window Height: " + request.height);
+				console.log("Window Width: " + request.width);
+				console.log("Tab_Id: " + Tab_Id);
+				chrome.tabs.remove(Tab_Id);
+			}
+		}
+	);
+
 };
 
 (function() {   //アイコンクリックで測定開始
 	chrome.browserAction.onClicked.addListener(start);
 }) ();
 
-chrome.runtime.onMessage.addListener(   //content_scriptからメッセージを受信すると実行
-  function (request, sender, sendResponse) {
-	  if(request.msg == "sending..."){
-		  chrome.tabs.sendMessage(Tab_Id, {msg: "sending..."}, function(response){
-			  console.log("finished!");
-		  });
-	  };
-	  if(request.msg == "finished"){
-		  console.log(request.result + "%");
-	  }
-  }
-);
+/*chrome.runtime.onMessage.addListener(   //content_scriptからメッセージを受信すると実行
+	function (request, sender, sendResponse) {
+		if(request.msg == "sending..."){
+			chrome.tabs.sendMessage(Tab_Id, {msg: "sending..."}, function(response){
+				console.log("finished!");
+			});
+		} else if(request.msg == "finished"){	//結果を表示
+			console.log("Energy Consumption: " + request.result + "%");
+			console.log("Window Height: " + request.height);
+			console.log("Window Width: " + request.width);
+			chrome.tabs.remove(Tab_Id);
+		}
+	}
+);*/
 
 
 
--- content_script.js
+++ content_script.js
@@ -1,8 +1,11 @@
-var showBattery = function(){
+chrome.runtime.sendMessage({msg: "sending..."}, function(response){	//background.jsにメッセージを送信
+});
+
+var showBattery = function(){	//background.jsからメッセージを受信すると実行開始
 
 	var before,after;	//計測前後のバッテリーを記憶
 //	var timer = 1 * 60000;	//計測する時間(ミリ秒指定)
-	var timer = 5000;	//5秒
+	var timer = 5000;	//5秒(デバッグ用)
 
 	navigator.getBattery().then(function(b){
 		console.log(b.level * 100 + "%");
@@ -14,10 +17,15 @@ var showBattery = function(){
 			console.log(b.level * 100 + "%");
 			after = b.level * 100;	//計測終了時のバッテリー
 			var result = after - before;
-			//alert("The difference is " + result + "%.");	//計測結果をalertで表示
-			chrome.runtime.sendMessage({msg: "finished", result: result}, function(response){
-				console.log("showBattery has finished!");
-			});
+			chrome.runtime.sendMessage(			//計測結果をメッセージで送信
+				{	msg: "finished",
+					result: result,
+					height: getHeight(),
+					width: getWidth()
+				}, function(response){
+
+				}
+			);
 		});
 	};
 
@@ -25,13 +33,17 @@ var showBattery = function(){
 
 };
 
-chrome.runtime.sendMessage({msg: "sending..."}, function(response){	//background.jsにメッセージを送信
-});
-
-
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
 	    console.log('received!!');
 	    showBattery();
 	}
 );
+
+var getHeight = function() {	//ウィンドウサイズを表示
+	return(document.body.clientHeight);	//高さ
+};
+
+var getWidth = function() {
+	return(document.body.scrollWidth);	//幅
+}
