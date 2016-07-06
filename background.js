8d42de5fab34e7b606976a642e8d673e6e2617fa
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-07-06 12:10:06 +0900

    modified:   background.js 	develop: using an array "url_list" to get url from a json file.

--- background.js
+++ background.js
@@ -8,40 +8,24 @@ var start = function() {
 
 	var url = getNextUrl();
 
-	/*chrome.runtime.getPackageDirectoryEntry(function(entry) {	//indexファイルの読み込み
+	chrome.runtime.getPackageDirectoryEntry(function(entry) {	//indexファイルの読み込み
 		entry.getFile('index.json', {create: false}, function(file_entry) {
 			file_entry.file(function(file){
-				var reader = new FileReader;
+				var reader = new FileReader();
 				reader.onload = function(event){	//ファイルのreadが完了すると実行
 					url_list = JSON.parse(event.target.result);	//urlのリストを配列に格納
+					chrome.tabs.create(
+		        {url: url_list[1]},
+		        function(tab) {
+			        Tab_Id = tab.id;
+		        }
+	        );
 				};
 				reader.readAsText(file, 'utf-8');
 			});
 		});
-	});*/	//将来の拡張のための準備
+	});	//将来の拡張のための準備
 
-	chrome.tabs.create(
-		{url: url},
-		function(tab) {
-			Tab_Id = tab.id;
-		}
-	);
-
-	/*chrome.runtime.onMessage.addListener(   //content_scriptからメッセージを受信すると実行
-		function (request, sender, sendResponse) {
-			if(request.msg == "sending..."){
-				chrome.tabs.sendMessage(sender.tab.id, {msg: "sending..."}, function(response){
-
-					console.log("sender: " + sender);
-				});
-			} else if(request.msg == "finished"){	//結果を表示
-				console.log("Energy Consumption: " + request.result + "%");
-				console.log("Window Height: " + request.height);
-				console.log("Window Width: " + request.width);
-				chrome.tabs.remove(sender.tab.id);	//計測が終了したタブを閉じる
-			}
-		}
-	);*/
 };
 
 chrome.runtime.onMessage.addListener(   //content_scriptからメッセージを受信すると実行
