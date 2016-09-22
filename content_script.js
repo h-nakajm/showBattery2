db0f80a0bc2f2f77c47a64dfb28f052eea872a8f
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-09-22 17:20:01 +0900

    modified:   .gitignore 	modified:   content_script.js 	timer changed(start_waiting, finish_waiting).

--- content_script.js
+++ content_script.js
@@ -15,6 +15,14 @@ document.addEventListener("DOMContentLoaded", function domcl(event) {
 //document.addEventListener("loaded", function loaded(event){
 window.onload = function nkjm(){	//画像まで読み込み終わると実行
 
+	var count = 0;
+	while($('img').length > count) {
+		if($('img').eq(count).css('font-family').match(/nakajm/)) {
+//			console.log('sachio');
+		}
+		count++;
+	}
+
 	chrome.runtime.sendMessage(	//計測開始時に，devtoolsにgetHARを開始させる
 		{
 			msg: "getHAR"
@@ -28,8 +36,8 @@ window.onload = function nkjm(){	//画像まで読み込み終わると実行
 //	var timer = 60 * 60000;	//計測する時間(ミリ秒指定)
 	var timer = 5000;	//5秒(デバッグ用)
 
-	var countup = function(){
-			stopwatch.finished = new Date();
+	/*var countup = function(){
+			stopwatch.finish_waiting = new Date();
 
 			var result = {	//データベースに結果を格納
 				url:document.location.href,
@@ -62,7 +70,7 @@ window.onload = function nkjm(){	//画像まで読み込み終わると実行
 			});
 	};
 
-	setTimeout(countup, timer);	//countupをtimer時間後に実行
+	setTimeout(countup, timer);	//countupをtimer時間後に実行*/
 
 };
 
@@ -78,9 +86,52 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	//console.log(JSON.parse(request));
 	console.log(request.tab_created);
 	console.log(sender);
+
 	if(request.tab_created != null) {
 		stopwatch.tab_created = new Date(request.tab_created);
 	} else {
+		//	var timer = 60 * 60000;	//計測する時間(ミリ秒指定)
+		var timer = 5000;	//5秒(デバッグ用)
+
+
+		// getHARが終わったら計測開始
 		har = JSON.parse(request);
+		stopwatch.start_waiting = new Date();
+
+		var countup = function(){
+			stopwatch.finish_waiting = new Date();
+
+			var result = {	//データベースに結果を格納
+				url:document.location.href,
+				document_body_clientHeight:getHeight(),
+				document_body_scrollWidth:getWidth(),
+				html:document.getElementsByTagName('html')[0].innerHTML,
+				har: har,
+				stopwatch: stopwatch
+			};
+			$.ajax({
+				url:"https://127.0.0.1:4443/nkjm/result/",
+				type:"POST",
+				contentType:"application/json",
+				data:JSON.stringify(result)
+			});
+			a = stopwatch;
+
+			console.log(stopwatch.start.toISOString());
+			console.log(stopwatch.dom_content_loaded.toISOString());
+			console.log(stopwatch.onload.toISOString());
+			console.log(stopwatch.finish_waiting.toISOString());
+			$(document).ajaxComplete(function(){	//ajax通信が完了すると実行
+				chrome.runtime.sendMessage(	//計測終了をbackground.jsに伝える
+					{
+						msg: "finished"
+					}, function(response){
+
+					}
+				);
+			});
+		};
+
+		setTimeout(countup, timer);	//countupをtimer時間後に実行
 	}
 });
