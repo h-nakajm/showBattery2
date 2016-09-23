739744074e9a71ef796f96868b023776357d0727
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-09-23 14:24:59 +0900

    modified:   content_script.js 	write element blocked ads into DB.

--- content_script.js
+++ content_script.js
@@ -34,44 +34,22 @@ window.onload = function nkjm(){	//画像まで読み込み終わると実行
 
 	stopwatch.onload = new Date();
 
-//	var timer = 60 * 60000;	//計測する時間(ミリ秒指定)
-	var timer = 5000;	//5秒(デバッグ用)
-
-	/*var countup = function(){
-			stopwatch.finish_waiting = new Date();
-
-			var result = {	//データベースに結果を格納
-				url:document.location.href,
-				document_body_clientHeight:getHeight(),
-				document_body_scrollWidth:getWidth(),
-				html:document.getElementsByTagName('html')[0].innerHTML,
-				har: har,
-				stopwatch: stopwatch
-			};
+	// labeledな広告を選択，DBに書き込み
+	$('*').each(function(i, d) {
+		if ($(d).css('font-family').match(/__ELEMENT__/)) {
+			console.log(d);
+			var ads = {};
+			ads.url = d.src;
+			ads.width = d.width;
+			ads.height = d.height;
 			$.ajax({
-				url:"https://127.0.0.1:4443/nkjm/result/",
+				url:"https://127.0.0.1:4443/nkjm/element_blocked/",
 				type:"POST",
 				contentType:"application/json",
-				data:JSON.stringify(result)
+				data:JSON.stringify(ads)
 			});
-			a = stopwatch;
-
-			console.log(stopwatch.start.toISOString());
-			console.log(stopwatch.dom_content_loaded.toISOString());
-			console.log(stopwatch.onload.toISOString());
-			console.log(stopwatch.finished.toISOString());
-			$(document).ajaxComplete(function(){	//ajax通信が完了すると実行
-				chrome.runtime.sendMessage(	//計測終了をbackground.jsに伝える
-					{
-						msg: "finished"
-					}, function(response){
-
-					}
-				);
-			});
-	};
-
-	setTimeout(countup, timer);	//countupをtimer時間後に実行*/
+		}
+	});
 
 };
 
